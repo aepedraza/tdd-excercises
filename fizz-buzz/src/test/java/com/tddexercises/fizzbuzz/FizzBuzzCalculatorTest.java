813853ea3842c7a5b0570898ec3c604357110e3d
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2023-10-20 14:07:37 -0300

    FizzBuzz - Refactor: Given input = 0 then return empty string
    Extracted constants and improved test readability

--- fizz-buzz/src/test/java/com/tddexercises/fizzbuzz/FizzBuzzCalculatorTest.java
+++ fizz-buzz/src/test/java/com/tddexercises/fizzbuzz/FizzBuzzCalculatorTest.java
@@ -1,15 +1,19 @@
 package com.tddexercises.fizzbuzz;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FizzBuzzCalculatorTest {
+
+    private static final String EMPTY_STRING = "";
 
     private final FizzBuzzCalculator underTest = new FizzBuzzCalculator();
 
     @Test
     void givenZeroAsInput_whenCalculateFizzBuzz_thenReturnEmptyString() {
         String result = underTest.fizzBuzz(0);
-        Assertions.assertEquals("", result);
+
+        assertEquals(EMPTY_STRING, result);
     }
 }
