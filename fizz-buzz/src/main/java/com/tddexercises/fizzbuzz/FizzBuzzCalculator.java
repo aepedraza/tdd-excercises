3c55e570ae17e24d2204ad67976cb446d41ca29d
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2023-10-21 15:10:15 -0300

    FizzBuzz - Refactor: Given input = 5N (multiple of 5), then return "Buzz"
    Extracted method and constants

--- fizz-buzz/src/main/java/com/tddexercises/fizzbuzz/FizzBuzzCalculator.java
+++ fizz-buzz/src/main/java/com/tddexercises/fizzbuzz/FizzBuzzCalculator.java
@@ -5,6 +5,8 @@ public class FizzBuzzCalculator {
     private static final String EMPTY_STRING_DEFAULT_VALUE = "";
     private static final String FIZZ_VALUE = "Fizz";
     private static final String BUZZ_VALUE = "Buzz";
+    private static final int FIZZ_MOD_DIVISOR = 3;
+    private static final int BUZZ_MOD_DIVISOR = 5;
 
     public String fizzBuzz(int input) {
         if (matchesFizzCondition(input)) {
@@ -19,10 +21,14 @@ public class FizzBuzzCalculator {
     }
 
     private boolean matchesFizzCondition(int input) {
-        return input > 0 && input % 3 == 0;
+        return matchesModCondition(input, FIZZ_MOD_DIVISOR);
+    }
+
+    private boolean matchesModCondition(int input, int mod) {
+        return input > 0 && input % mod == 0;
     }
 
     private boolean matchesBuzzCondition(int input) {
-        return input > 0 && input % 5 == 0;
+        return matchesModCondition(input, BUZZ_MOD_DIVISOR);
     }
 }
