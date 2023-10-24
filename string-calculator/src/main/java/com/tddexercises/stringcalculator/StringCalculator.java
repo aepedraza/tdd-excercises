7c8e3bbaf9d719c69a38c2692a44f09a1976630f
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2023-10-24 10:27:59 -0300

    String calculator - Green: Given single number then return the same number

--- string-calculator/src/main/java/com/tddexercises/stringcalculator/StringCalculator.java
+++ string-calculator/src/main/java/com/tddexercises/stringcalculator/StringCalculator.java
@@ -2,6 +2,10 @@ package com.tddexercises.stringcalculator;
 
 public class StringCalculator {
     public int add(String input) {
-        return 0;
+        if (input.isEmpty()) {
+            return 0;
+        } else {
+            return Integer.parseInt(input);
+        }
     }
 }
