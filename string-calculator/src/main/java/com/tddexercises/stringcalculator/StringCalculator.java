a1a4bf907068c0c78ea71440f22ba04f67ffa4ba
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2023-10-24 11:10:46 -0300

    String calculator - Refactor: Given input with different separators, then allow and return their sum
    Improved regex build

--- string-calculator/src/main/java/com/tddexercises/stringcalculator/StringCalculator.java
+++ string-calculator/src/main/java/com/tddexercises/stringcalculator/StringCalculator.java
@@ -1,16 +1,16 @@
 package com.tddexercises.stringcalculator;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class StringCalculator {
 
+    private static final String COMMA_SEPARATOR = ",";
+    private static final String NEWLINE_SEPARATOR = "\n";
+    private static final String REGEX_OR_CONDITION = "|";
     private static final int EMPTY_STRING_RESULT = 0;
-    private static final String SEPARATOR = ",";
 
     public int add(String input) {
-        String[] split = input.split(SEPARATOR + "|" + "\n");
+        String[] split = input.split(String.join(REGEX_OR_CONDITION, COMMA_SEPARATOR, NEWLINE_SEPARATOR));
 
         if (split.length == 1) {
             return input.isEmpty() ? EMPTY_STRING_RESULT : Integer.parseInt(input);
