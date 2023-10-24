48db27e9c2863e12a77f9cdef715f5f5d54e89e3
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2023-10-24 10:25:45 -0300

    String calculator - Red: Given single number then return the same number
    Added failing assertion with multiple input

--- string-calculator/src/test/java/com/tddexercises/stringcalculator/StringCalculatorTest.java
+++ string-calculator/src/test/java/com/tddexercises/stringcalculator/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 package com.tddexercises.stringcalculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,7 +12,15 @@ class StringCalculatorTest {
     private final StringCalculator underTest = new StringCalculator();
 
     @Test
+    @DisplayName("Given empty string then return zero")
     void givenEmptyString_thenReturnZero() {
         assertEquals(0, underTest.add(""));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Given single number as input then return the same number")
+    @ValueSource(strings = { "1", "2", "3" })
+    void givenSingleNumber_thenReturnSameNumber(String input) {
+        assertEquals(Integer.parseInt(input), underTest.add(input));
     }
 }
