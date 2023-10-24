4a7e3ac1ebbac1f4633e6c415b48abafbbe276df
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2023-10-24 10:37:57 -0300

    String calculator - Refactor: Given 2 numbers separated by comma, then return their sum
    Extracted constants

--- string-calculator/src/main/java/com/tddexercises/stringcalculator/StringCalculator.java
+++ string-calculator/src/main/java/com/tddexercises/stringcalculator/StringCalculator.java
@@ -1,11 +1,15 @@
 package com.tddexercises.stringcalculator;
 
 public class StringCalculator {
+
+    private static final int EMPTY_STRING_RESULT = 0;
+    private static final String SEPARATOR = ",";
+
     public int add(String input) {
         if (input.isEmpty()) {
-            return 0;
+            return EMPTY_STRING_RESULT;
         } else {
-            String[] split = input.split(",");
+            String[] split = input.split(SEPARATOR);
             if (split.length == 1) {
                 return Integer.parseInt(input);
             } else {
